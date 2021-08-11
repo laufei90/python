9aeb4f1a9c03678a7f2f820d4c9eec4d767898d4
laufei90 <51823824+laufei90@users.noreply.github.com>
2021-08-11 13:59:55 +0800

    Add files via upload

---
+++ ping_and_mail_use_logging.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+#
+
+import subprocess
+import yagmail
+import time
+import logging
+import logging.config
+
+logging.config.fileConfig('logging.cnf')
+
+tv_ip="192.168.15.185"
+is_on_watching=0
+start_time=int(time.time())
+on_time=int(time.time())
+end_time=int(time.time())
+
+
+def to_mail(content_str):
+    yag = yagmail.SMTP(user="****@163.com", password="F***P", host='smtp.163.com')
+    subject = "看电视提醒"
+    contents = content_str
+    email_name = "niufee@qq.com"
+    yag.send(email_name, subject, contents)
+    yag.close()
+
+while True :
+    if subprocess.call("ping -c 1 -w 1 %s" % tv_ip,shell = True,stdout = open('/dev/null','w'))==0:
+        #alived
+        if  is_on_watching == 0 :
+            start_time=int(time.time())
+            logging.warning('开始看电视')
+        else :
+            on_time=int(time.time())
+
+        is_on_watching=1
+
+    else:
+         #unreacheable
+        if  is_on_watching == 1 :
+            end_time=int(time.time())
+            logging.warning('停止看电视')
+
+        is_on_watching=0
+
+    cal_watch_time=on_time-start_time
+    #print("看电视时间：{}s".format(cal_watch_time))
+    if cal_watch_time >= 3600 :
+       to_mail("你家的娃看电视超过一个小时了,开始时间:{}.".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))))
+
+    time.sleep(60)
+
+
+
